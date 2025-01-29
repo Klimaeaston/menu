b32356a857632c46cce993cf52208ae77c861de0
easton <eklima000@stu.waukesha.k12.wi.us>
2025-01-29 08:41:16 -0600

    Initial Commit

---
+++ menu/menuApp.swift
@@ -0,0 +1,17 @@
+//
+//  menuApp.swift
+//  menu
+//
+//  Created by Klima, Easton C on 1/29/25.
+//
+
+import SwiftUI
+
+@main
+struct menuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ menuTests/menuTests.swift
@@ -0,0 +1,17 @@
+//
+//  menuTests.swift
+//  menuTests
+//
+//  Created by Klima, Easton C on 1/29/25.
+//
+
+import Testing
+@testable import menu
+
+struct menuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
